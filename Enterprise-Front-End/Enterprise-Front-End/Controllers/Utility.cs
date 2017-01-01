47bc6a92751f41fc13acd62306cc86f79ac4a9d4
Miguel Saavedra <chownema@gmail.com>
2017-01-02 11:46:48 +1300

    Added Resource Files and references

---
+++ Enterprise-Front-End/Enterprise-Front-End/Controllers/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enterprise_Front_End.Controllers
+{
+    public static class Utility
+    {
+    }
+}
---
+++ Enterprise-Front-End/Enterprise-Front-End/Views/Page1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Enterprise_Front_End
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        Boolean Blur = false;
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Blur)
+            {
+                this.Effect = null;
+                Blur = false;
+            }
+            else
+            {
+                this.Effect = new System.Windows.Media.Effects.BlurEffect();
+                Blur = true;
+            }
+
+
+        }
+    }
+}
