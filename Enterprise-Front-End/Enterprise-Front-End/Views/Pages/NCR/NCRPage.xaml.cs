7cc6016bbcd7719b20e68e63cf2b439ae3f0c47e
Scentefy <vampirebane@gmail.com>
2017-01-05 15:27:34 +1300

    datagrid implemented

--- Enterprise-Front-End/Enterprise-Front-End/Views/Pages/NCR/NCRPage.xaml.cs
+++ Enterprise-Front-End/Enterprise-Front-End/Views/Pages/NCR/NCRPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows;
 
 namespace Enterprise_Front_End.Views.Pages.NCR
 {
@@ -23,6 +25,40 @@ namespace Enterprise_Front_End.Views.Pages.NCR
         public NCRPage()
         {
             InitializeComponent();
+            List<Data> data = new List<Data>();
+            data.Add(new Data() { Skew = 1, IssueTitle = "Miguel", ManufactureDate = new DateTime(1971, 7, 23), Status = "Approved" });
+            data.Add(new Data() { Skew = 2, IssueTitle = "Fingered", ManufactureDate = new DateTime(1974, 1, 17), Status = "Approved" });
+            data.Add(new Data() { Skew = 5, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 9, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 9, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 3, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 3, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 7, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 73, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 2, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 6, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 8, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 9, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 365, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 367, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 32, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 345, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 23, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+            data.Add(new Data() { Skew = 1, IssueTitle = "The yoghurt", ManufactureDate = new DateTime(1991, 9, 2), Status = "Approved" });
+
+            ncrData.ItemsSource = data;
         }
+
+    }
+
+    public class Data
+    {
+        public int Skew { get; set; }
+
+        public string IssueTitle { get; set; }
+
+        public DateTime ManufactureDate { get; set; }
+
+        public string Status { get; set; }
     }
 }
