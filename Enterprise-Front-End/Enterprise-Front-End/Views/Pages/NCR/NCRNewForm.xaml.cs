d16617f9189048377a6351b752e2f6deeb0027f6
Scentefy <vampirebane@gmail.com>
2017-01-18 16:36:15 +1300

    implemented Datepicker to the new NCR form. currently working on generating NCR object

--- Enterprise-Front-End/Enterprise-Front-End/Views/Pages/NCR/NCRNewForm.xaml.cs
+++ Enterprise-Front-End/Enterprise-Front-End/Views/Pages/NCR/NCRNewForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Enterprise_Front_End.Models;
 
 namespace Enterprise_Front_End.Views.Pages.NCR
 {
@@ -23,6 +25,53 @@ namespace Enterprise_Front_End.Views.Pages.NCR
         public NCRNewForm()
         {
             InitializeComponent();
+        }
+
+        public void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= TextBox_GotFocus;
+        }
+
+        //public void generateNCR(object sender, RoutedEventArgs e)
+        //{
+        //    string output = ManufactureDateTextBox.Text.Substring(ManufactureDateTextBox.Text.IndexOf('/') + 1);
+
+        //    manfactDatePicker.Text
+        //        manfactDatePicker.Value.ToString("yyyy-MM-dd");
+
+        //    DateTime dt = DateTime.ParseExact(manfactDatePicker.DisplayDate, "ddMMyyyy",
+        //                          CultureInfo.InvariantCulture);
+        //    dt.ToString("yyyyMMdd");
+
+        //    NCRObject ncrObj = new NCRObject
+        //        (skewText.Text,
+        //        DateTime.Now.ToString(),
+        //        costTextBox.Text,
+        //        manfactDatePicker.Text,
+        //        manfactDatePicker.Text,
+        //        bbDatePicker.Text);
+        //}
+
+        private void DatePicker_SelectedDateChanged(object sender,
+           SelectionChangedEventArgs e)
+        {
+            // ... Get DatePicker reference.
+            var picker = sender as DatePicker;
+
+            // ... Get nullable DateTime from SelectedDate.
+            DateTime? date = picker.SelectedDate;
+            if (date == null)
+            {
+                // ... A null object.
+                this.Title = "No date";
+            }
+            else
+            {
+                // ... No need to display the time.
+                this.Title = date.Value.ToShortDateString();
+            }
         }
 
     }
